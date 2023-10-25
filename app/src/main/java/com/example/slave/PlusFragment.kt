83735eef6a5babd0a940ece1131b523c29fb99a8
fprtmjinho <fjrtmjinho@gmail.com>
2023-10-25 15:47:03 +0900

    fix: add name.text and number.text exception handling

--- app/src/main/java/com/example/slave/PlusFragment.kt
+++ app/src/main/java/com/example/slave/PlusFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.slave.databinding.FragmentPlusBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +19,31 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PlusFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentPlusBinding? = null
+    private lateinit var mainActivity: MainActivity
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plus, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlusFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlusFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        _binding = FragmentPlusBinding.inflate(inflater, container, false)
+        mainActivity = context as MainActivity
+        binding.plusBtn.setOnClickListener{
+            if(binding.name.text.toString()==""){
+                Toast.makeText(mainActivity,"이름을 입력해주세요.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+            if(binding.number.text.toString()==""){
+                Toast.makeText(mainActivity,"전화번호를 입력해주세요.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            saveNumberBook(binding.name.text.toString(),binding.number.text.toString())
+        }
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+    private fun saveNumberBook(name: String, number: String){
+
     }
 }
