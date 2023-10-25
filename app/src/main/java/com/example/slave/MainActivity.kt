0aa42c72a3481cf60838e7bd1fa25cd6591a9e5a
fprtmjinho <fjrtmjinho@gmail.com>
2023-10-25 14:36:56 +0900

    feat: start slave and add MainActivity.kt, PlusFragment.kt, MenuFragment.kt

---
+++ app/src/main/java/com/example/slave/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.slave
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.slave.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+    private fun initBottomNavigation(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, PlusFragment())
+            .commit()
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.plusFragment -> {
+                    val homeFragment = PlusFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frame, homeFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.menuFragment -> {
+                    val lookFragment = MenuFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frame, lookFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+}
